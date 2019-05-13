5bfdbdf0b69e91b7339d3a39d658f87e31926172
L 4 I K A <powermusic969@gmail.com>
2019-05-13 19:29:50 -0400

    Bases para la utilización de Javascript en unn documento html empleando <script>

---
+++ Convertidor de grados celcius a farenheith.js
@@ -0,0 +1,2 @@
+var cTemp = 100; //Declara una variable cTem, que lleva consigo la temperatura en celsius.
+var hTemp = ((cTemp * 9) / 5) + 32;
---
+++ Funciones.js
@@ -0,0 +1,8 @@
+function diHola() {
+	alert("Hola a todos");
+	//...Aquí peuden ir
+	//Otras declaraciones
+
+	function diHadios() {
+	alert("Adios");
+}
